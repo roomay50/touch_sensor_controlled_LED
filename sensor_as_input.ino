bc1ee69e1b9b393cc4608b982c09f399c142b103
Rosemary Agala <97768325+roomay50@users.noreply.github.com>
2026-06-03 07:55:21 +0100

    Added the .ino file

---
+++ sensor_as_input.ino
@@ -0,0 +1,54 @@
+int sensor = 3;
+int led1 = 6;
+int led2 = 9;
+int led3 = 11;
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensor, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
+}
+
+void loop() {
+  bool sensor_state;
+  sensor_state = digitalRead(sensor);
+  Serial.print("sensor");
+  Serial.println(sensor_state);
+  if (sensor_state == LOW){
+    counter+=1;
+  }
+
+  switch(counter){
+    case 0:
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      break;
+    case 1:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      break;
+    case 2:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, LOW);
+      break;
+    case 3:
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      break;
+    case 4:
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      // break;
+    counter=0;
+  }
+  delay(500);
+}
